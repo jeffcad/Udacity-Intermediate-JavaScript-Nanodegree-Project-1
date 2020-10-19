83f3df925d5461ecc8dcd795f591ba0cc0830471
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-19 14:14:24 +0900

    Initial commit (with some spelling fixes)

---
+++ app.js
@@ -0,0 +1,32 @@
+
+    // Create Dino Constructor
+
+
+    // Create Dino Objects
+
+
+    // Create Human Object
+
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+    // NOTE: Weight in JSON file is in lbs, height in inches. 
+
+    
+    // Create Dino Compare Method 2
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+    
+    // Create Dino Compare Method 3
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Generate Tiles for each Dino in Array
+  
+        // Add tiles to DOM
+
+    // Remove form from screen
+
+
+// On button click, prepare and display infographic
